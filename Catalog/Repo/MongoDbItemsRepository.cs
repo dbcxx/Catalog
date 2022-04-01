c525bace5c635fae53e6234d079fc51449a8dbab
dolapo <bamideledolapo.s@gmail.com>
2022-04-01 06:30:40 +0100

    added mongodb

--- Catalog/Repo/MongoDbItemsRepository.cs
+++ Catalog/Repo/MongoDbItemsRepository.cs
@@ -1,59 +1,51 @@
-﻿//using Catalog.Entities;
-//using MongoDB.Bson;
-//using MongoDB.Driver;
-//using System;
-//using System.Collections.Generic;
-//using System.Threading.Tasks;
+﻿using Catalog.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-//namespace Catalog.Repo
-//{
-//    public class MongoDbItemsRepository : IInMemoryItemsRepository
-//    {
-//        private const string databaseName = "catalog";
-//        private const string collectionName = "items";
-//        private readonly IMongoCollection<Item> itemsCollection; 
+namespace Catalog.Repo
+{
+    public class MongoDbItemsRepository : IInMemoryItemsRepository
+    {
+        private const string databaseName = "catalog";
+        private const string collectionName = "items";
+        private readonly IMongoCollection<Item> itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
 
-//        public MongoDbItemsRepository(IMongoClient mongoClient)
-//        {
-//            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
-//            itemsCollection = database.GetCollection<Item>(collectionName);
-//        }
-//        public async Task CreateItemsAsync(Item item)
-//        {
-//            await itemsCollection.InsertOneAsync(item);
-//        }
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+            itemsCollection = database.GetCollection<Item>(collectionName);
+        }
 
-//        public async Task DeleteItemAsync(Guid id)
-//        {
-//            var filter = filterBuilder.Eq(item => item.Id, id);
-//            await itemsCollection.DeleteOneAsync(filter);
-//        }
+        public void CreateItem(Item item)
+        {
+            itemsCollection.InsertOne(item);
+        }
 
-//        public async Task<Item> GetItemAsync(Guid id)
-//        {
-//            var filter = filterBuilder.Eq(item => item.Id, id);
-//            return await itemsCollection.Find(filter).SingleOrDefaultAsync();
-//        }
+        public void DeleteItem(Guid id)
+        {
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            itemsCollection.DeleteOne(filter);
+        }
 
-//        public async Task<IEnumerable<Item>> GetItemsAsync()
-//        {
-//            return await itemsCollection.Find(new BsonDocument()).ToListAsync();
-//        }
+        public Item GetItem(Guid id)
+        {
+            var filter = filterBuilder.Eq(item => item.Id, id);
+            return itemsCollection.Find(filter).SingleOrDefault();
+        }
 
-//        public Task<Item> GetItemsAsync(Guid id)
-//        {
-//            throw new NotImplementedException();
-//        }
+        public IEnumerable<Item> GetItems()
+        {
+            return itemsCollection.Find(new BsonDocument()).ToList();
+        }
 
-//        public async Task UpdateItemAsync(Item item)
-//        {
-//            var filter = filterBuilder.Eq(existingItem => existingItem.Id, item.Id);
-//            await itemsCollection.ReplaceOneAsync(filter, item);
-//        }
-
-//        public Task UpdateItemsAsync(Item item)
-//        {
-//            throw new NotImplementedException();
-//        }
-//    }
-//}
+        public void UpdateItem(Item item)
+        {
+            var filter = filterBuilder.Eq(existingItem => existingItem.Id, item.Id);
+            itemsCollection.ReplaceOne(filter, item);
+        }
+    }
+}
--- Catalog/Startup.cs
+++ Catalog/Startup.cs
@@ -1,4 +1,5 @@
 using Catalog.Repo;
+using Catalog.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,6 +9,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,8 +32,15 @@ namespace Catalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
-            services.AddSingleton<IInMemoryItemsRepository, InMemoryItemsRepository>();
+            services.AddSingleton<IMongoClient>(ServiceProvider =>
+            {
+                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+                return new MongoClient(settings.ConnectionString);
+            });
+            services.AddSingleton<IInMemoryItemsRepository, MongoDbItemsRepository>();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
